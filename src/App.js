190d34695a2698fdc25eeb19d7368cae1349234a
Lucas Pugnali <pugnali.lucas@gmail.com>
2019-10-11 00:47:34 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import SPARQLQueryDispatcher from './Service.js';
 import './App.css';
 
+
 function App() {
+  
+  const initialState = {
+    name: "",
+    image: "",
+    nacimiento: "",
+    age: 0,
+    realAge: null
+  };
+  const [person, setPerson] = useState(initialState);
+  const initialLoad = ()=>{
+    setPerson(initialState);
+    const endpointUrl = 'https://query.wikidata.org/sparql';
+    const sparqlQuery = `SELECT ?item ?itemLabel ?DR ?image ?RIP
+    WHERE 
+    {
+      ?item wdt:P27 wd:Q414.
+      ?item wdt:P569 ?DR . # P569 : Date of birth
+      ?item wdt:P18 ?image .
+      OPTIONAL{?item wdt:P570 ?RIP .}     # P570 : Date of death
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+    limit 10000`;
+    
+    const queryDispatcher = new SPARQLQueryDispatcher(endpointUrl);
+    queryDispatcher.query(sparqlQuery).then((res) =>{
+      var ripped = res.results.bindings.filter(x => x.RIP == null);
+      console.dir(ripped);
+      var rnd = ripped[Math.floor(Math.random() * ripped.length)];
+      debugger;
+      setPerson({
+        name: rnd.itemLabel.value,
+        nacimiento: rnd.DR.value,
+        image: rnd.image.value});
+      console.dir(rnd);
+    });
+    
+  };
+  
+  const getAge = (dateString) => {
+      var today = new Date();
+      var birthDate = new Date(dateString);
+      var age = today.getFullYear() - birthDate.getFullYear();
+      var m = today.getMonth() - birthDate.getMonth();
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      return age;
+  }
+
+  const loadItem = () => {
+    initialLoad();
+  }
+
+  const handleChange = (e) => {
+    setPerson({...person,age: e.target.value});
+  }
+
+  const validate = () => {
+    setPerson({ ...person, realAge: getAge(new Date(person.nacimiento))})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div style={{width:'100%'}}>
+        <img style={{width:'100%'}} src={person.image} alt=""/>
+        </div>
+        <div style={{width: '100%',
+    position: 'fixed',
+    float: 'left',
+    backgroundColor: '#8080806b',
+    bottom: '0',
+    padding: '10px'}}>
+        <button style={{display: (person.name == "" && person.realAge == null) || person.realAge != null  ? "inline" : "none"}} onClick={loadItem}>{`${person.realAge != null ? "Volver a" : ""} Jugar`}</button>
+        <h3>{person.name}</h3>
+        <input type="number" value={person.age} onChange={handleChange}/>
+        <button onClick={validate}>Probar</button>
+        <h2 style={{display: person.realAge == null?"none" : "block"}}>{person.realAge == person.age ? "CORRECTO" : `INCORRECTO, tiene ${person.realAge}`}</h2>
+      </div>
+      </div>
     </div>
   );
 }
