e28403e32a0bdbdf66cd075b58f708b8ad5468ad
Lucas Pugnali <pugnali.lucas@gmail.com>
2019-10-11 00:54:35 -0300

    Updates

---
+++ precache-manifest.b418a31422fe2df621ba1523e8abc038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13152eb05bf1da7281a55cf8ea7c3158",
+    "url": "/guess-their-age-arg/index.html"
+  },
+  {
+    "revision": "9a24f71e2f8e15367035",
+    "url": "/guess-their-age-arg/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "713f28688b24e71c31e9",
+    "url": "/guess-their-age-arg/static/js/2.ae5c7139.chunk.js"
+  },
+  {
+    "revision": "9a24f71e2f8e15367035",
+    "url": "/guess-their-age-arg/static/js/main.0b79b101.chunk.js"
+  },
+  {
+    "revision": "a3e1480778042bc01d7e",
+    "url": "/guess-their-age-arg/static/js/runtime-main.ff3d6234.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/guess-their-age-arg/static/media/logo.25bf045c.svg"
+  }
+]);
